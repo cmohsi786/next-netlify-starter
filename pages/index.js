0c4c6064377ffba43b4d483f125574db150e4731
cmohsi786 <mohsin786.ch@gmail.com>
2023-08-08 16:09:11 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,10 +15,10 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-<p>I am normal</p>
-<p style="color:red;">I am red</p>
-<p style="color:blue;">I am blue</p>
-<p style="font-size:50px;">I am big</p>
+        <p>I am normal</p>
+        <p style="color:red;">I am red</p>
+        <p style="color:blue;">I am blue</p>
+        <p style="font-size:50px;">I am big</p>
       </main>
 
       <Footer />
